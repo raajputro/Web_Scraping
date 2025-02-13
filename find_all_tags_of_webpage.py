ec5afd9c5c2f097e942c60ae359280faa5ee1825
raajputro <34886621+raajputro@users.noreply.github.com>
2025-02-14 00:49:26 +0600

    scraping all pages

--- find_all_tags_of_webpage.py
+++ find_all_tags_of_webpage.py
@@ -2,6 +2,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+parent_url = 'https://www.scrapethissite.com'
 url = 'https://www.scrapethissite.com/pages/'
 
 def scrape_page(given_url):
@@ -10,17 +11,31 @@ def scrape_page(given_url):
         soup = BeautifulSoup(response.text, "html.parser")
 
         tags = soup.find_all(True)
-        # print("All tags found in the page:")
-        # for tag in tags:
-        #     print(tag.name)
 
-        # with open("tags.txt", "w") as f:
-        #     for tag in tags:
-        #         f.write(f"{tag.name}\n")
-        #         print("Tags and values written to tags.txt file")
-        page_title = soup.title.string()
-        with open(f"{page_title}.json", "w") as json_file:
+        page_title = soup.find('title').text
+        file_name =page_title.split('|')[0] +".json"
+        with open(file_name, "w") as json_file:
             json.dump([{tag.name: tag.text.strip()} for tag in tags], json_file)
 
     except requests.exceptions.RequestException as e:
         print("Failed!",e)
+
+def find_sub_pages(given_url):
+    try:
+        response = requests.get(given_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        links = soup.find_all("a",href=True)
+        print("Links found in the page:")
+        for link in links:
+            print(link['href'])
+        return links
+
+    except requests.exceptions.RequestException as e:
+        print("Failed!",e)
+
+links = find_sub_pages(url)
+
+for link in links:
+    print(parent_url+link['href'])
+    scrape_page(parent_url+link['href'])
